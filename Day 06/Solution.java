a2fc4cc31f5d4eb8935833eca89f0fe1abaa35e4
bhaktichopra2 <bhakti.aps@gmail.com>
2025-09-14 20:18:48 +0530

    Day 6 Completed

--- Day 06/Solution.java
+++ Day 06/Solution.java
@@ -2,7 +2,7 @@
 
 // Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
 
-public class Solution {
+public class Solution{
     public int minSubArrayLen(int target, int[] nums) {
         int res = Integer.MAX_VALUE;
         int l = 0;
