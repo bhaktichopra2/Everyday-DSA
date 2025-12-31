dc49aed5b46c167627580e34038b5a315abed3bc
bhaktichopra2 <bhakti.aps@gmail.com>
2025-12-31 18:46:01 +0530

    Day 87 Completed

---
+++ Day 87/Solution.py
@@ -0,0 +1,41 @@
+# 31. Next Permutation
+# A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+# For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+# The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+# For example, the next permutation of arr = [1,2,3] is [1,3,2].
+# Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+# While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+# Given an array of integers nums, find the next permutation of nums.
+
+# The replacement must be in place and use only constant extra memory.
+
+from typing import List
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        index = -1
+        
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] < nums[i+1]:
+                index = i
+                break
+        
+        if index == -1:
+            nums.reverse()
+            return
+        
+        for i in range(len(nums)-1, index, -1):
+            if nums[i] > nums[index]:
+                nums[i], nums[index] = nums[index], nums[i]
+                break
+        
+        nums[index+1:] = reversed(nums[index+1:])
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [1, 2, 3]
+    print(f"Before: {nums}")
+    sol.nextPermutation(nums)
+    print(f"After:  {nums}")
