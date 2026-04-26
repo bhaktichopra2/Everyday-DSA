46c06b95b917fbf7b7f6c595671e0f676e4c4dd2
bhaktichopra2 <bhakti.aps@gmail.com>
2026-04-26 14:55:00 +0530

    Day 193 Completed

---
+++ Day 193/Solution27.java
@@ -0,0 +1,36 @@
+// 27. Remove Element
+
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+import java.util.*;
+
+public class Solution27 {
+
+    public static int removeElement(int[] nums, int val) {
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+
+        int k = removeElement(nums, val);
+
+        System.out.println(k);
+        System.out.println(Arrays.toString(Arrays.copyOf(nums, k)));
+    }
+}
